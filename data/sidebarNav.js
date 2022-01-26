4ba8424b74c33f6c12f0dd5daeeedc3e7af5a9d2
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-01-26 22:16:55 +0500

    finish setup

---
+++ data/sidebarNav.js
@@ -0,0 +1,22 @@
+export const sidebarNav = [
+    {
+        link: '/',
+        section: 'home',
+        text: 'Home'
+    },
+    {
+        link: '/orders',
+        section: 'orders',
+        text: 'Orders'
+    },
+    {
+        link: '/products',
+        section: 'products',
+        text: 'Products'
+    },
+    {
+        link: '/customers',
+        section: 'customers',
+        text: 'Customers'
+    },
+]
